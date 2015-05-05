1c93ce07454400a30afdd4742b82dc8f387c540e
Jeffery Chen Fan <jeffery9@gmail.com>
2015-05-05 11:51:39 +0800

    更新构建脚本

--- scripts/build_i18n.sh
+++ scripts/build_i18n.sh
@@ -12,8 +12,8 @@ if [ ! -d $HOME_DIR/_build ]; then
     mkdir $HOME_DIR/_build
 fi
 
-if [ ! -d $HOME_DIR/function ]; then
-    mkdir $HOME_DIR/function
+if [ ! -d $HOME_DIR/bundle ]; then
+    mkdir $HOME_DIR/bundle
 fi
 
 if [ ! -d $HOME_DIR/odoo ]; then
@@ -47,28 +47,28 @@ python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=
 
 done
 
-python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/function/account.po -l zh_CN --modules=account,account_accountant,account_analytic_analysis,account_analytic_default,account_analytic_plans,account_anglo_saxon,account_asset,account_bank_statement_extensions,account_budget,account_cancel,account_chart,account_check_writing,account_followup,account_payment,account_sequence,account_test,account_voucher,analytic,purchase_analytic_plans,sale_analytic_plans
+python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/bundle/account.po -l zh_CN --modules=account,account_accountant,account_analytic_analysis,account_analytic_default,account_analytic_plans,account_anglo_saxon,account_asset,account_bank_statement_extensions,account_budget,account_cancel,account_chart,account_check_writing,account_followup,account_payment,account_sequence,account_test,account_voucher,analytic,purchase_analytic_plans,sale_analytic_plans
 
-python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/function/product.po -l zh_CN --modules=product,product_email_template,product_expiry,product_extended,product_margin,product_visible_discount
+python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/bundle/product.po -l zh_CN --modules=product,product_email_template,product_expiry,product_extended,product_margin,product_visible_discount
 
-python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/function/stock.po -l zh_CN --modules=stock,stock_account,stock_dropshipping,stock_invoice_directly,stock_landed_costs,stock_picking_wave,sale_stock,procurement,procurement_jit,procurement_jit_stock
+python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/bundle/stock.po -l zh_CN --modules=stock,stock_account,stock_dropshipping,stock_invoice_directly,stock_landed_costs,stock_picking_wave,sale_stock,procurement,procurement_jit,procurement_jit_stock
 
-python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/function/project.po -l zh_CN --modules=project,project_issue,project_issue_sheet,project_timesheet
+python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/bundle/project.po -l zh_CN --modules=project,project_issue,project_issue_sheet,project_timesheet
 
-python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/function/hr.po -l zh_CN --modules=hr,hr_applicant_document,hr_attendance,hr_contract,hr_evaluation,hr_expense,hr_gamification,hr_holidays,hr_payroll,hr_payroll_account,hr_recruitment,hr_timesheet,hr_timesheet_sheet
+python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/bundle/hr.po -l zh_CN --modules=hr,hr_applicant_document,hr_attendance,hr_contract,hr_evaluation,hr_expense,hr_gamification,hr_holidays,hr_payroll,hr_payroll_account,hr_recruitment,hr_timesheet,hr_timesheet_sheet
 
-python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/function/mrp.po -l zh_CN --modules=mrp,mrp_byproduct,mrp_operations,mrp_repair,sale_mrp
+python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/bundle/mrp.po -l zh_CN --modules=mrp,mrp_byproduct,mrp_operations,mrp_repair,sale_mrp
 
-python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/function/base.po -l zh_CN --modules=base,web,web_analytics,web_api,web_calendar,web_diagram,web_gantt,web_graph,web_kanban,web_kanban_gauge,web_kanban_sparkline
+python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/bundle/base.po -l zh_CN --modules=base,web,web_analytics,web_api,web_calendar,web_diagram,web_gantt,web_graph,web_kanban,web_kanban_gauge,web_kanban_sparkline
 
-python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/function/website.po -l zh_CN --modules=website,website_blog,website_certification,website_crm,website_crm_partner_assign,website_customer,website_event,website_event_sale,website_event_track,website_forum,website_forum_doc,website_gengo,website_google_map,website_hr,website_hr_recruitment,website_instantclick,website_livechat,website_mail,website_mail_group,website_membership,website_partner,website_payment,website_project,website_quote,website_report,website_sale,website_sale_delivery,website_sale_options,website_twitter
+python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/bundle/website.po -l zh_CN --modules=website,website_blog,website_certification,website_crm,website_crm_partner_assign,website_customer,website_event,website_event_sale,website_event_track,website_forum,website_forum_doc,website_gengo,website_google_map,website_hr,website_hr_recruitment,website_instantclick,website_livechat,website_mail,website_mail_group,website_membership,website_partner,website_payment,website_project,website_quote,website_report,website_sale,website_sale_delivery,website_sale_options,website_twitter
 
-python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/function/zh_CN.po -l zh_CN 
+python $OE_DIR/openerp-server -c $CONFIG --stop-after-init -d $DB --i18n-export=$HOME_DIR/bundle/zh_CN.po -l zh_CN 
 
 cp -rf $HOME_DIR/odoo/addons/base  $HOME_DIR/odoo/openerp/addons/
 rm -rf $HOME_DIR/odoo/addons/base
 cp -rf $HOME_DIR/odoo  $HOME_DIR/_build/
-cp -rf $HOME_DIR/function  $HOME_DIR/_build/
+cp -rf $HOME_DIR/bundle  $HOME_DIR/_build/
 
 cd $HOME_DIR/_build/ 
 find .  -type f -size -450c -delete
